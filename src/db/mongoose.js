b1544c185ac5c488b56ff291dac2d80e34ee0781
Rajan Bhatia <rajanbhatia100@gmail.com>
2021-09-14 14:11:28 +0530

    New

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,50 +1,14 @@
-const mongoose=  require('mongoose')
+const mongoose=  require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL,{
-    useNewUrlParser:true,
+const DBURL = 'mongodb+srv://taskmanager:Rajan0707@cluster1.ktqs8.mongodb.net/test?authSource=admin&replicaSet=atlas-14gkqu-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 
+mongoose.connect(DBURL,{
+    useNewUrlParser:true,  
+})
+.then(()=>{
+    console.log("database connected")
+})
+.catch((error)=>{
+    console.log("connection error:=>",error)
 })
 
-// const User = mongoose.model('User',{
-//     name:{
-//         type:String
-//     },
-//     age:{
-//         type:Number
-
-//     }
-// })
-
-// const me = new User({
-//     name:'Rajan',
-//     age:23
-// })
-
-// me.save().then(()=>{
-//     console.log(me);
-// }).catch((error)=>{
-//     console.log('errorrr!!!1..',error)
-// })
-
-// const Task = mongoose.model('Task',{
-//     name:{
-//         type:String
-//     },
-//     age:{
-//         type:Number
-//     },
-//     phone:{
-//         type:Number
-//     }
-// })
-
-// const myTask = new Task({
-//     name:"Rajan",
-//     age:23,
-//     phone:8295501695
-// })
-// myTask.save().then(()=>{
-//     console.log(myTask)
-// }).catch((error)=>{
-//     console.log('errrooorrrr!!!..',error)
-// })
